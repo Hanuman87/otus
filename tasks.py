8c45a287a42f8201d509eaf6f32b7e2a6f884d74
Hanuman87 <wsharay@yandex.ru>
2025-09-04 19:15:32 +0300

    feat: add dvc

--- tasks.py
+++ tasks.py
@@ -1,6 +1,20 @@
 from invoke import task
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 @task
 def setup(ctx):
     ctx.run("uv sync", pty=True)
     ctx.run("uv python install 3.11.12", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("echo $TEST_ENV", pty=True)
+
+
+@task
+def setup_dvc(ctx):
+    ctx.run("dvc init", pty=True)
+    ctx.run("bash setup-dvc.sh", pty=True)
